c8d96ee03081b3bfc4d02da23345f2ec1f418545
Ekazhu91 <116168173+Ekazhu91@users.noreply.github.com>
2022-10-20 12:52:59 +0300

    Add files via upload
    Лабораторная 1 тема 2.
    Лаб 2 тема 3

---
+++ Hometasks/main.task1.py
@@ -0,0 +1,6 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+unique_number = set(list_)
+
+print(sum(unique_number))
+print(len(unique_number))
+print(round(unique_number) - )
