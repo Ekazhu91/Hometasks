bec3d5ddc1661ed16d844e756b66ebfab9e4e8c5
Ekazhu91 <116168173+Ekazhu91@users.noreply.github.com>
2022-10-26 00:18:20 +0300

    Add files via upload
    Добавила 2 лабораторные по теме 3. 
    Сейчас разбираюсь с 2 заданиями с циклами. скину завтра/послезавтра.
    Не понимаю как решить.

---
+++ Тема 3/main1.py
@@ -0,0 +1,11 @@
+src = not False and True or False and not True
+
+ # result =  not False or True and False
+ # result = True and False
+ # result = True
+
+# TODO расписать упрощение выражения
+
+result = True  # TODO подставить результат выражения
+
+print(src == result)
