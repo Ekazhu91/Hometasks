9ea042ede3675c09847b272ec7a53152508abba0
Ekazhu91 <116168173+Ekazhu91@users.noreply.github.com>
2022-10-31 01:50:13 +0300

    Add files via upload

--- Тема 3/main2.py
+++ Тема 3/main2.py
@@ -4,7 +4,8 @@ list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 3
 list_numbers[9] = 25
 list_numbers[-1] = 90
 
-
+i_max = list_numbers.index(max(list_numbers))
+list_numbers[i_max], list_numbers[-1] = list_numbers[-1], list_numbers[i_max]
 
 
 
